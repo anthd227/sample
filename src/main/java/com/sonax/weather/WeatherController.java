594ea9e3a03bc1a66a9ca77fa592bc10ff2ccd5f
HeeJinYu <62230031+HeeJinYu@users.noreply.github.com>
2020-12-23 17:53:08 +0900

    기상청 날씨 api 가져와서 파싱

--- src/main/java/com/sonax/weather/WeatherController.java
+++ src/main/java/com/sonax/weather/WeatherController.java
@@ -6,9 +6,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.collections4.map.HashedMap;
 import org.apache.commons.io.IOUtils;
@@ -23,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 
 @Controller
@@ -34,47 +40,7 @@ public class WeatherController {
 	@RequestMapping("/weatherPage.do")
 	public String weatherPage() {
 		try {
-			URL url = new URL("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1159068000");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			
-			/*BufferedReader br =  new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String txt = "";
-			while((txt = br.readLine()) != null) {
-				System.out.println(txt);
-			}*/
-			// conn.getResponseCode() == 200 && conn.getInputStream() != null 
-			InputStream input = conn.getInputStream();
-			
-			String xml = IOUtils.toString(input);
-			JSONObject jObj = XML.toJSONObject(xml);
-			/*String jsontxt = jObj.toString(4);
-			System.out.println(jsontxt);
-			*/
-			String jsonStr = jObj.toString();
-			
-			ObjectMapper mapper = new ObjectMapper();
-			
-			Map<String, Object> map = new HashedMap<>();
-			map = mapper.readValue(jsonStr, new TypeReference<Map<String,Object>>(){});
-			Map<String, Object> rss = (Map<String, Object>) map.get("rss");
-			//Map<String, Object> body = (Map<String, Object>) rss.get("rss");
-			Map<String, Object> channel = (Map<String, Object>) rss.get("channel");
-			Map<String, Object> item = (Map<String, Object>) channel.get("item");
-			Map<String, Object> description = (Map<String, Object>) item.get("description");
-			Map<String, Object> body = (Map<String, Object>) description.get("body");
-			Map<String, Object> data = (Map<String, Object>) body.get("data");
-			
-			List<Map<String, Object>> weatherData = new ArrayList<>();
-			
-			weatherData.add(data);
-			System.out.println(data);
-			/*
-			 * 전체 출력
-			 * mapper.enable(SerializationFeature.INDENT_OUTPUT);
-			Object json = mapper.readValue(jObj.toString(), Object.class);
-			String output = mapper.writeValueAsString(json);
-			System.out.println(output);*/
-	
+			getWeather("1132068000", "local");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -83,24 +49,50 @@ public class WeatherController {
 		return "weather";
 	}
 	
-	public JSONObject xmlToJson(HttpURLConnection conn) {
-		try {
-			
-			BufferedReader br =  new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String txt = "";
-			String xmlTxt = "";
-			while((txt = br.readLine()) != null) {
-				xmlTxt = xmlTxt.concat(txt);
-			}
-			InputStream input = conn.getInputStream();
-			JSONParser parser = new JSONParser();
-			JSONObject jObj = (JSONObject) parser.parse(xmlTxt);
-			
-			System.out.println(jObj.toString());
-		} catch (IOException | ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void getWeather(String localCode, String type) throws IOException {
+		String urlTxt = "";
+		if(type.equals("midTerm")) {
+			urlTxt = "https://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=";
+		} else {
+			urlTxt = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=";
 		}
-		return null;
+		URL url = new URL(urlTxt + localCode);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		
+		if(conn.getResponseCode() == 200 && conn.getInputStream() != null) {
+			InputStream input = conn.getInputStream();
+			
+			String xml = IOUtils.toString(input);
+			JSONObject jObj = XML.toJSONObject(xml);
+			
+			String jsonStr = jObj.toString();
+			ObjectMapper mapper = new ObjectMapper();
+			Map<String, Object> map = new HashedMap<>();
+			map = mapper.readValue(jsonStr, new TypeReference<Map<String,Object>>(){});
+			
+			//전체출력
+			mapper.enable(SerializationFeature.INDENT_OUTPUT);
+			Object json = mapper.readValue(jObj.toString(), Object.class);
+			String output = mapper.writeValueAsString(json);
+			System.out.println(output);
+			
+			Map<String, Object> rss = (Map<String, Object>) map.get("rss");
+			Map<String, Object> channel = (Map<String, Object>) rss.get("channel");
+			Map<String, Object> item = (Map<String, Object>) channel.get("item");
+			Map<String, Object> description = (Map<String, Object>) item.get("description");
+			Map<String, Object> body = (Map<String, Object>) description.get("body");
+			
+			List<Map<String, Object>> data = null;
+			if(type.equals("midTerm")) {
+				data = (List<Map<String, Object>>) body.get("location");
+			} else {
+				data = (List<Map<String, Object>>) body.get("data"); 
+			}
+			
+			for(int ii = 0; ii < data.size(); ii++) {
+				Map<String, Object> weatherData = data.get(ii);
+				System.out.println(weatherData);
+			}
+		}
 	}
 }
